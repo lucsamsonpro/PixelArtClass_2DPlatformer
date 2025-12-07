d1ad96d0533f18f7782a01f0ff0d0323d98b8696
lucsamsonpro <luc.samson.pro@gmail.com>
2025-12-07 01:53:12 +0100

    WIP 2025-12-06

---
+++ PixelArtClass_2DPlatformer_LucSamson/Assets/Tiled/AutoTileMap_LucSamson/PixelArtClass_Tileset_LucSamson.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="PixelArtClass_Tileset_LucSamson" tilewidth="32" tileheight="32" tilecount="45" columns="9">
+ <image source="PixelArtClass_Tileset_LucSamson.png" width="288" height="160"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
